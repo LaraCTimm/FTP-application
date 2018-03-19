d3b736e32d6f3650a172a3f5879af3506b21d33f
Sasha Berkowitz <sashabii@hotmail.com>
2018-03-19 15:52:44 +0200

    Print functionality to terminal window

--- newInterface.py
+++ newInterface.py
@@ -19,16 +19,23 @@ def run(event):
 
 # START SERVER
 # *TO DO* Check if server is running OR exit server with exit of app
-os.system("start cmd /k python FTPserver.py")
+#os.system("start cmd /k python FTPserver.py")
 
 # START CLIENT
 def login():
 	if userEntry.get() == 'Username' or passEntry.get() == 'Password' or addressEntry.get() == 'ServerAddress':
 		tkMessageBox.showinfo("Login Error", "Please enter a username, password and address.")
 	else:
-		print 'Entered username:' + userEntry.get() + '\n'
-		print 'Entered password:' + passEntry.get() + '\n'
-		print 'Entered address:' + addressEntry.get() + '\n'
+		terminalText.insert('1.0', "Entered username:" + userEntry.get()+"\n")
+		terminalText.pack()
+		terminalText.insert('1.0', "Entered password:" + passEntry.get()+"\n")
+		terminalText.pack()
+		terminalText.insert('1.0', "Entered address:" + addressEntry.get()+"\n")
+		terminalText.pack()
+		#print 'Entered username:' + userEntry.get() + '\n'
+		#print 'Entered password:' + passEntry.get() + '\n'
+		#print 'Entered address:' + addressEntry.get() + '\n'
+		
 		# To be replaced with login functions
 
 
@@ -39,25 +46,32 @@ userEntry = Entry(window)
 userEntry.insert(0,"Username")
 userEntry.grid(row=0,column=0,padx=10,pady=10)
 
+# Password Entry
 passEntry = Entry(window)
 passEntry.insert(0,"Password")
 passEntry.grid(row=0,column=1,padx=5,pady=10)
 
+# Address Entry
 addressEntry = Entry(window)
 addressEntry.insert(0,"ServerAddress")
 addressEntry.grid(row=0,column=2,padx=10,pady=10)
   
+# Connect Button
 connectBtn = Button(window, text="CONNECT", command=login)
 connectBtn.grid(row=0,column=3,padx=50)
 
-serverFrame = Frame(width=fileSysX, height=fileSysY,  colormap="new",relief = SUNKEN,borderwidth=2,bg='white')
+serverFrame = Frame(width=fileSysX, height=fileSysY,  colormap="new",relief = SUNKEN,borderwidth=2,bg='')
 serverFrame.grid(row=1,column=0,columnspan=2,padx=framePadX,pady=framePadY)
 
-localFrame = Frame(width=fileSysX, height=fileSysY, colormap="new",relief = SUNKEN,borderwidth=2,bg='white')
+localFrame = Frame(width=fileSysX, height=fileSysY, colormap="new",relief = SUNKEN,borderwidth=2,bg='')
 localFrame.grid(row=1,column=2,columnspan=2,padx=framePadX,pady=framePadY)
 
-terminalFrame = Frame(colormap="new",relief = SUNKEN,borderwidth=2,height=180,width=(2*fileSysX)+(4*framePadX),bg='black')
+terminalFrame = Frame(colormap="new",relief = SUNKEN,borderwidth=2,bg='black')
 terminalFrame.grid(row=2,column=0,columnspan=4,padx=framePadX,pady=framePadY)
+terminalText = Text(terminalFrame,bg='black',fg='white',height=14,width=75)
+terminalText.pack()
+#terminalText.insert('1.0', "Just a new text Widget\nin two lines\n")
+#terminalText.pack()
 
 terminalEntry = Entry(window,width=102,bg='black',fg='white')
 terminalEntry.insert(0,">Enter custon command>>")
@@ -65,3 +79,14 @@ terminalEntry.grid(row=3,column=0,columnspan=4)
 terminalEntry.bind('<Return>',run)
 
 window.mainloop()
+
+
+
+
+# buffer = io.StringIO()
+
+# print("something", file=buffer)
+# print("something more", file=buffer)
+
+# output = buffer.getvalue()
+# text.insert(END, output)
