bef976f221b277f98521accfa0569dbb9ac7c36f
LaraCTimm <l_timm@hotmail.com>
2018-03-19 10:41:13 +0200

    more comments, bug fix

--- FTPserver.py
+++ FTPserver.py
@@ -106,8 +106,11 @@ class clientThread(threading.Thread):
                 if index != -1:
                     self.userRow = i    # identify which user is connecting to server
                     break               # stop looking
-                    
-            if self.userRow > -1:       # if username is found in userData file
+            
+            storedUsername = (self.userData[self.userRow].split(' '))[0]
+
+            if storedUsername == self.username:       # if username matches that found in user data file
+
                 self.baseDirectory = os.path.join(serverDirectory, self.username) 
 
                 if os.path.isdir(self.baseDirectory):
@@ -131,9 +134,10 @@ class clientThread(threading.Thread):
 
         if password != '' and password != ' ':      # must have entered a valid password
             if self.username != '':                 # must have entered a registered username 
+
                 if self.userRow > -1:               # if the username was found in the userData file
-                    # retrieve stored password
-                    storedPassword = self.userData[self.userRow][-(len(password)+1):-1] 
+                    # retrieve stored password (minus end of line character)
+                    storedPassword = (self.userData[self.userRow].split(' '))[1][:-1]
 
                     if storedPassword == password:
                         self.loggedIn = True
@@ -163,10 +167,6 @@ class clientThread(threading.Thread):
         index = directoryString.find('\\')
         if index != -1:
             directoryString = directoryString.split('\\')[-1]   # isolate directory name
-
-        # if directoryString == self.workingDirectory.split('\\')[-2]:
-        #     self.CDUP('')
-        #     return
         
         # make a path of the directory to be changed to
         newDirectory = os.path.join(self.workingDirectory, directoryString)
@@ -232,7 +232,7 @@ class clientThread(threading.Thread):
         self.passiveSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.passiveSocket.bind((locIP, 0))     # binds to local IP and available port
         ip, port = self.passiveSocket.getsockname()
-        self.passiveSocket.listen(1)
+        self.passiveSocket.listen(1)            # listen for a new connection on specified port
         print 'Listening on',ip,':',port
 
         # convert details to connection string to be sent to client
@@ -260,7 +260,7 @@ class clientThread(threading.Thread):
             self.connSock.send('504 Command not implemented for that parameter.\r\n')
 
     def STRU(self, command):
-        # specifies file structure (file[D], record, page) ONLY DEFAULT IMPLEMENTED
+        # specifies file structure. Only default, File, is implemented
         if self.checkLoggedIn() == False:
             return
 
@@ -272,7 +272,7 @@ class clientThread(threading.Thread):
             self.connSock.send('504 Command not implemented for that parameter.\r\n')
 
     def MODE(self, command):
-        # specify data transfer mode (stream[D], block, compressed) ONLY DEFAULT IMPLEMENTED
+        # specify data transfer mode. Only default, Stream, is implemented
         if self.checkLoggedIn() == False:
             return
 
@@ -285,21 +285,26 @@ class clientThread(threading.Thread):
 
     # service commands ---------------------------------------------------------------------------------
 
-    def RETR(self, command):
-        # transfer a copy file to client 
+    def RETR(self, command):    # transfer a copy file to client 
+
         if self.checkLoggedIn() == False:
             return
 
+        # only continue if a data socket can be opened
         if self.setPortPasv: 
             fileName = command[5:-2]
-            index = fileName.find('\\')
+
+            # check for variations in commands made by different clients
+            index = fileName.find('\\')     
             if index != -1:
                 fileName = fileName.split('\\')[-1]
                 
             filePath = os.path.join(self.workingDirectory, fileName)
             
-            if os.path.isfile(filePath):
+            if os.path.isfile(filePath):    # if file exists on server
                 requestedFile = None
+
+                # open the file to read, open mode depends on the file type set by TYPE command 
                 if self.binaryFile:
                     requestedFile = open(filePath,'rb')
                 else:
@@ -309,18 +314,18 @@ class clientThread(threading.Thread):
                 try:
                     self.open_dataSocket()
 
-                    fileChunk = requestedFile.read(1024)
+                    fileChunk = requestedFile.read(1024)        # read from the file
 
-                    while fileChunk:
+                    while fileChunk:                            # while there is still more file to read
                         print 'Sending...'
-                        self.dataSocket.send(fileChunk)
-                        fileChunk = requestedFile.read(1024)
+                        self.dataSocket.send(fileChunk)         # send on dataSocket
+                        fileChunk = requestedFile.read(1024)    # read some more
                 
                 except Exception, err:
                     self.connSock.send('451 Requested action aborted: local error in processing.\r\n')
 
-                requestedFile.close()
-                self.close_dataSocket()
+                requestedFile.close()       # close the file read from
+                self.close_dataSocket()     # close the data socket
 
                 self.connSock.send('226 Closing data connection. Requested file action successful.\r\n')
             else:
@@ -328,21 +333,24 @@ class clientThread(threading.Thread):
         else:
             self.connSock.send('425 Use PORT or PASV first.\r\n')
 
-    def STOR(self, command):
-        # accept data from data connection and store as file on server
+    def STOR(self, command):    # accept data from data connection and store as file on server
+        
         if self.checkLoggedIn() == False:
             return
-    
-        if self.setPortPasv:
 
+        # only continue if a data socket can be opened
+        if self.setPortPasv:
             fileName = command[5:-2]
+
+            # check for variations in commands made by different clients
             index = fileName.find('\\')
             if index != -1:
                 fileName = fileName.split('\\')[-1]
 
             filePath = os.path.join(self.workingDirectory, fileName)
-
             requestedFile = None
+
+            # open the file to write, open mode depends on the file type set by TYPE command
             if self.binaryFile:
                 requestedFile = open(filePath, 'wb')
             else:
@@ -353,15 +361,16 @@ class clientThread(threading.Thread):
             try:
                 self.open_dataSocket()
 
-                fileChunk = self.dataSocket.recv(1024)
-                while (fileChunk):
+                fileChunk = self.dataSocket.recv(1024)      # recieve from data socket
+                while (fileChunk):                          # while the data recieved exists
                     print "Receiving..."
-                    requestedFile.write(fileChunk)
-                    fileChunk = self.dataSocket.recv(1024)
+                    requestedFile.write(fileChunk)          # write what is recieved to file
+                    fileChunk = self.dataSocket.recv(1024)  # continue to recieve from data socket
 
-                requestedFile.close()
-                self.close_dataSocket()
                 print "Done Receiving"
+                requestedFile.close()           # close the file read from
+                self.close_dataSocket()         # close the data socket
+
                 self.connSock.send('226 Closing data connection. Requested file action successful.\r\n')
             
             except Exception, err:
@@ -372,36 +381,40 @@ class clientThread(threading.Thread):
     def open_dataSocket(self):
         #----PASSIVE-------------------------------------------------------
         if self.passive:
-            self.dataSocket, addr = self.passiveSocket.accept()
-            print 'Data stream opened at address:', addr
+            self.dataSocket, addr = self.passiveSocket.accept()     # accept a connection from client
+            print 'Data stream opened at address:', addr           
         #----ACTIVE--------------------------------------------------------
         else:
-            self.dataSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.dataSocket.connect((self.dataAddress,self.dataPort))   # params retreived from PORT command
+            self.dataSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # make a new TCP socket
+            self.dataSocket.connect((self.dataAddress,self.dataPort))   # bind to address/port recieved from PORT command
             print 'Data stream opened at address: (\'%s\', %u)' % (self.dataAddress, self.dataPort)
 
     def close_dataSocket(self):
-        self.setPortPasv = False
-        if self.passive:
+        self.setPortPasv = False        # require a new PORT/PASV command to be made before more data can be sent
+        if self.passive:                # close passive socket (if passive)
             self.passiveSocket.close()
-        self.dataSocket.close()
+        self.dataSocket.close()         # close the data socket
     
     def DELE(self, command):
         if self.checkLoggedIn() == False:
             return
 
         fileString = command[5:-2]
-        index = fileString.find('\\')
 
+        # check for variations in commands made by different clients
+        index = fileString.find('\\')
         if index != -1:
             fileString = fileString.split('\\')[-1]
 
         filePath = os.path.join(self.workingDirectory, fileString)
-        if os.path.isfile(filePath):
+
+        if os.path.isfile(filePath):    # if the file exists, delete it
             os.remove(filePath)
             self.connSock.send('250 Requested file action okay, file deleted.\r\n')
-        elif os.path.isdir(filePath):
+
+        elif os.path.isdir(filePath):   # to remove directory use a different command
             self.connSock.send('550 Requested action not taken. To delete directory use RMD.\r\n')
+        
         else:
             self.connSock.send('550 Requested action not taken. File does not exist.\r\n')
 
@@ -409,49 +422,59 @@ class clientThread(threading.Thread):
         if self.checkLoggedIn() == False:
             return
 
+        # only send the part of the directory path from the client base directory onwards
         self.connSock.send('257 \"%s\" is the working directory.\r\n' % (self.workingDirectory[len(serverDirectory):]))
 
     def LIST(self, command):
         if self.checkLoggedIn() == False:
             return
+        
+        # ensure that the list is sent in ASCII mode
         self.binaryFile = False
 
-        if self.setPortPasv:
-            self.connSock.send('150 Opening data connection. Sending directory list.\r\n')    
-            self.open_dataSocket()
+        if self.setPortPasv:    # check data stream ready for opening
 
-            # print self.workingDirectory
+            self.connSock.send('150 Opening data connection. Sending directory list.\r\n')    
+            self.open_dataSocket()      # open data socket
+
             itemString = ''
 
+            # for all items in the working directory, get their file info (createItemString) and append it to the item string
             for item in os.listdir(self.workingDirectory):
                 itemString += self.createItemString(os.path.join(self.workingDirectory,item))
 
-            self.dataSocket.send(itemString)
+            self.dataSocket.send(itemString)    # send the directory list in the data socket 
             
             self.connSock.send('226 Closing data connection. Directory list sent.\r\n')
-            self.close_dataSocket()
+            self.close_dataSocket()     # close data socket
         else:
             self.connSock.send('425 Use PORT or PASV first.\r\n')
 
     def createItemString(self, itemPath):
-        itemStat = os.stat(itemPath)
-        permissionString = 'rwxrwxrwx'
-        itemPermissions = ''
-        directoryChar = ''
+        itemStat = os.stat(itemPath)        # using stat module, get file info
+        permissionString = 'rwxrwxrwx'      # set maximum permission string
+        itemPermissions = ''                # clear item permissions
+        directoryChar = ''                  # clear directory flag
 
+        # for each char in permissionString, check and set permission based on result from 
+        #       the logical AND of that bit (of the file mode) and 1
         for i in range(9):
             if (itemStat.st_mode>>(8-i)) & 1:
                 itemPermissions += permissionString[i]
             else:
                 itemPermissions += '-'
 
+        # if the item is a directory, set the directory flag
         if os.path.isdir(itemPath):
             directoryChar = 'd'
         else:
             directoryChar = '-'
 
+        # extract the date and time values 
         timestamp = time.strftime("%b %d %H:%M" , time.localtime(itemStat[ST_MTIME]))
 
+        # assemble the itemString for that item
+        # permissionString itemNumber userID groupID fileSize timeStamp filePath
         itemString = directoryChar + itemPermissions + ' ' + str(itemStat[ST_INO]) + ' ' + \
                      str(itemStat[ST_UID]) + ' ' + str(itemStat[ST_GID]) + ' ' + \
                      str(itemStat[ST_SIZE]) + ' ' + timestamp + ' ' + os.path.basename(itemPath) + '\r\n'
@@ -463,19 +486,22 @@ class clientThread(threading.Thread):
             return
 
         directoryString = command[4:-2]
-        index = directoryString.find('\\')
 
+        # check for variations in commands made by different clients
+        index = directoryString.find('\\')
         if index != -1:
             directoryString = directoryString.split('\\')[-1]
 
         dirPath = os.path.join(self.workingDirectory, directoryString)
-        if not os.path.exists(dirPath):
+
+        if not os.path.exists(dirPath):     # if the directory doesn't already exist, make it
             os.makedirs(dirPath)
+            # send the part of the path higher that the users base directory
             self.connSock.send('257 \"%s\\%s\" created.\r\n'\
-                    % (self.workingDirectory[len(serverDirectory):],command[4:-2]))
+                    % (self.workingDirectory[len(serverDirectory):], command[4:-2]))
         else:
             self.connSock.send('550 Requested action not taken. \"%s\\%s\" already exists.\r\n'\
-                    % (self.workingDirectory[len(serverDirectory):],command[4:-2]) )
+                    % (self.workingDirectory[len(serverDirectory):], command[4:-2]) )
             
     def RMD(self, command):
         if self.checkLoggedIn() == False:
@@ -484,16 +510,20 @@ class clientThread(threading.Thread):
         if command[5:] != '':
             directoryString = command[4:-2]
 
+            # check for variations in commands made by different clients
             index = directoryString.find('\\')
             if index != -1:
                 directoryString = directoryString.split('\\')[-1]
 
             dirPath = os.path.join(self.workingDirectory, directoryString)
-            if os.path.isdir(dirPath):
+
+            if os.path.isdir(dirPath):      # if the directory exists, delete it
                 shutil.rmtree(dirPath)
                 self.connSock.send('250 Requested file action okay, directory deleted.\r\n')
-            elif os.path.isfile(dirPath):
+
+            elif os.path.isfile(dirPath):   # if the requested item is a file, use a dirrerent command
                 self.connSock.send('550 Requested action not taken. To delete file use DELE\r\n')
+
             else:
                 self.connSock.send('550 Requested action not taken. Directory does not exist.\r\n')
         else:
